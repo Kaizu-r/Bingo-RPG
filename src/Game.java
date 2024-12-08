03f7652453baddb7b758f75c6d9b5009df02c0f0
Kaizu-r <jljoloc@gmail.com>
2024-12-08 21:11:00 +0800

    UPD: Changed board to 6x6 UPD: Changed roll behavior to make it more fair UPD: Made board look better

--- src/Game.java
+++ src/Game.java
@@ -9,14 +9,16 @@ public class Game{
     public int[][] bingo;
     int rolls;
     public int totalRolls;
-    public int grassCount = 10;
+    public int grassCount = 18;
     public int eliteCount = 4;
-    public int enemyCount = 5; 
-    public int lootCount = 3;
-    public int shrineCount = 2;
+    public int enemyCount = 7; 
+    public int lootCount = 3;   
+    public int shrineCount = 0;
 
     public Game(String name){
-        bingo = new int[5][5];
+        bingo = new int[6][6];
+        shrineCount = 0;    //set this to 3 after a few rolls so it doesnt spawn immediately
+        enemyCount = 0;  //set to 7 after a few rolls
         this.name = name;
     } 
     //0 for locked, 1 grass, 2 enemy, 3 loot, 4 shrine, 5 elite, 6 boss
@@ -24,26 +26,42 @@ public class Game{
 
     //returns whatever tile it is getting
     public int roll(int a, int b){
-        if(totalRolls == 24){
+        if(totalRolls == 35){
             bingo[a][b] = 6;
             return 6;   //boss fight
         }
-        if(rolls == 4){ 
+        if(totalRolls == 3){
+            enemyCount = 7; //start spawning enemies
+        }
+        if(totalRolls == 6){
+            shrineCount = 3;    //start spawning shrines here
+        }
+        if((totalRolls + 1)%6 == 0 && totalRolls >= 11){  //elite only after 11 rolls cuz we not evil
             rolls = 0;
             bingo[a][b] = 5;   //elite
             eliteCount--;
             return 5;
         }
-
+        if(totalRolls%6 == 0 && totalRolls > 11){   //guaranteed shrine after elite
+            shrineCount--;
+            return 3;
+        }
         rolls++;
         totalRolls++;
         int ran;
         Random random = new Random();
                     //grass
-        int arr[] = {7, enemyCount, lootCount, shrineCount};   //contains all enemies here
+        int arr[] = {grassCount, enemyCount, lootCount};   //contains all enemies here
 
         do{
-            ran = random.nextInt(4);
+            ran = random.nextInt(9);
+            //playing with probabilities
+            if(ran < 5)
+                ran = 0;    //60% chance of grass
+            else if(ran < 8)
+                ran = 1;    //30% chance of enemy
+            else if(ran < 9)
+                ran = 2; //10% chance of loot
         }while(arr[ran] == 0);  //roll again if its out of the thing
 
         bingo[a][b] = ran + 1;
@@ -52,15 +70,22 @@ public class Game{
         grassCount = arr[0];
         enemyCount = arr[1];
         lootCount = arr[2];
-        shrineCount = arr[3];
 
         return ran + 1;
     }
 
     public void printGame(){
-        for(int i = 0; i < 5; i++){
+        System.out.print("                                               ");
+        System.out.println("         0        1         2         3         4        5");
+        System.out.println();
+        for(int i = 0; i < 6; i++){
             for(int k = 1; k < 5; k++){
-                for(int j = 0; j < 5; j++){
+                System.out.print("                                               ");
+                if(k == 3)
+                    System.out.print(i + "   ");
+                else
+                    System.out.print("    ");
+                for(int j = 0; j < 6; j++){
                     switch(bingo[i][j]){
                         case 0:
                             Blocks.locked(k);
