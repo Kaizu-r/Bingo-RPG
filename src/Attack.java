c3f171cdaefda4ea346fb0e32b4427207d4baf8a
Kaizu-r <jljoloc@gmail.com>
2024-12-09 01:02:23 +0800

    ADD: attack name

--- src/Attack.java
+++ src/Attack.java
@@ -3,9 +3,11 @@ package bingo.game;
 //attack component
 public class Attack{
     public int damage;
+    public String name;
     public int cost;
 
-    Attack(int damage, int cost){
+    Attack(String name, int damage, int cost){
+        this.name = name;
         this.damage = damage;
         this.cost = cost;
     }
