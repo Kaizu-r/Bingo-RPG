99636f479e78c94c77f4c636c163e9d1543374c2
Kaizu-r <jljoloc@gmail.com>
2024-12-09 00:56:58 +0800

    ADD: Attack component

---
+++ src/Attack.java
@@ -0,0 +1,12 @@
+package bingo.game;
+
+//attack component
+public class Attack{
+    public int damage;
+    public int cost;
+
+    Attack(int damage, int cost){
+        this.damage = damage;
+        this.cose = cost;
+    }
+}
