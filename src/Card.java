b6e5af8ecabf6ae9ff6e8b5d2f6748db1ee44d03
Kaizu-r <jljoloc@gmail.com>
2024-12-09 14:20:28 +0800

    ADD: Row, column, and diagonal checking ADD: Match resetter

---
+++ src/Card.java
@@ -0,0 +1,111 @@
+package bingo.game;
+
+import java.util.Random;
+
+
+public class Card{
+    public int bingo[][] = new int[5][5]; //working card
+    public int bingoOrig[][] = new int[5][5]; //original card, used for resetting
+    private int bingoCheck[][] = new int[5][5];
+    public static int randomNumber;
+    public boolean winner = false;
+    public static int selected[] = new int[75]; //1 to 75
+    public static int allNums[] = new int[75]; //all numbers in the card
+    public int matches;
+
+    public Card(){
+        //genarate numbers
+
+        Random ran = new Random();
+        
+        for(int i = 0, k = 0; i < 5; i++, k+= 15){
+            int arr[] = new int[15];
+            for(int j = 0; j < 5; j++){
+                int a;
+                do{
+                    a = ran.nextInt(15);
+                }while(arr[a] != 0);
+                arr[a] = 1;
+                bingo[i][j] = a + 1 + k;
+                bingoOrig[i][j] = a + 1 + k;
+                allNums[a + k]++;
+            }
+        }
+        allNums[bingo[2][2] - 1]--; //remove from list the middle number
+        bingo[2][2] = 0;    //remove middle for FREE
+    }
+
+    public void resetCard(){
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                bingo[i][j] = bingoOrig[i][j];
+            }
+        }
+        bingo[2][2] = 0; //free
+    }
+
+    public void resetMatch(){
+        this.matches = 0;   //after a move is finished
+    }
+    public static int draw(){
+        Random ran = new Random();
+
+        do{
+            randomNumber = ran.nextInt(75);
+        }while(selected[randomNumber] != 0 || allNums[randomNumber] == 0);
+        selected[randomNumber] = 1;
+
+        return randomNumber + 1;
+    }
+
+    public void match(){
+        //linearly search if it matches
+        int i = (randomNumber)/15;   //only look at the correct "letter"
+        for(int j = 0; j < 5; j++){
+            if(bingo[i][j] == randomNumber + 1){
+                bingoCheck[i][j] = 1;
+                bingo[i][j] = 0;
+                matches++;
+            }
+        }
+    }
+
+    public boolean checkRow(int i){
+        for(int j = 0; j < 5; j++){
+            if(bingo[i][j] != 0)
+                return false;
+        }
+        return true;
+    }   
+
+    public boolean checkColumn(int j){
+        for(int i = 0; i < 5; i++){
+            if(bingo[i][j] != 0)
+                return false;
+        }
+        return true;
+    }
+
+    public boolean checkDiagonal(){
+        for(int i = 0; i < 5; i++){
+            if(bingo[i][i] != 0)    //main diagonal
+                return false;
+            if(bingo[i][4 - i] != 0)    //other diagonal
+                return false;
+        }
+        return true;
+    }
+
+    public boolean isWinner(){
+        return winner;
+    }
+
+    public void printCard(){
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                System.out.print(bingo[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
