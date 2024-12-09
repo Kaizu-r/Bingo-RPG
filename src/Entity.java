6fa3f53efc2626006eafe3acde1ed4d089b7086f
Kaizu-r <jljoloc@gmail.com>
2024-12-09 14:42:08 +0800

    ADD: dodge mechanic

--- src/Entity.java
+++ src/Entity.java
@@ -1,5 +1,7 @@
 package bingo.game;
 
+
+
 public abstract class Entity{
     private Card card;
     public String[] sprite;
@@ -8,13 +10,32 @@ public abstract class Entity{
     public int damageBonus;
     public int hp;
     public int armor;
+    public boolean dodging;
+
+    public Entity(String name, int damageBonus, int hp, int armor, boolean alive){
+        this.name = name;
+        this.damageBonus = damageBonus;
+        this.hp = hp;
+        this.armor = armor;
+        this.alive = alive;
+        card = new Card();
+    }
 
     public void takeDamage(int damage){
         this.hp -= damage - (armor >> 1);
     }
 
+    public void dodge(){
+        dodging = true;
+    }
+    public void resetDodge(){
+        dodging = false;
+    }
     public int attack(int dmg){
         return dmg + (dmg * damageBonus);
+    }
+    public void die(){
+        alive = !(hp<= 0);
     }
     
 
