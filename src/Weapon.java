f845baea9a1fa1e28e87ef358d2bc7ea9df13d57
Kaizu-r <jljoloc@gmail.com>
2024-12-09 14:45:32 +0800

    REMOVE: card object

--- src/Weapon.java
+++ src/Weapon.java
@@ -4,13 +4,15 @@ import bingo.game.Item;
 import bingo.game.Attack;
 
 public class Weapon extends Item{
-    public String name;
     public Attack atk1;
     public Attack atk2;
 
     public Weapon(String name, Attack atk1, Attack atk2){
+        super(name);
         this.name = name;
         this.atk1 = atk1;
         this.atk2 = atk2;
     }
+
+    
 }
