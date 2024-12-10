6b1ab330fa8bef613ac6fcc4ece9d5557314ad69
Kaizu-r <jljoloc@gmail.com>
2024-12-11 00:16:57 +0800

    ADD: player move

--- src/Player.java
+++ src/Player.java
@@ -1,27 +1,75 @@
 package bingo.game;
 
 import java.util.ArrayList;
+import java.io.IOException;
 import bingo.game.Item;
 import bingo.game.Weapon;
 import bingo.game.Entity;
 
 public class Player extends Entity{
     private ArrayList<Item> inventory;
+    private int potionCount;
     private Item[] equipped;
     private ArrayList<Weapon> equipment;
     private Weapon weapon;
     public int luck;    //for luck stuff
 
-    public Player(char type, String name, int dmgBonus, int hp, int armor){
+    public Player(String name, int dmgBonus, int hp, int armor) throws IOException{
         super(name, dmgBonus, hp, armor, true);
         inventory = new ArrayList<Item>();
         equipment = new ArrayList<Weapon>();
         equipped = new Item[3];
+        potionCount = 2;
        
     }
 
     public void pickup(Item item){
         inventory.add(item);
+    }
+
+    public boolean canMove(int move){
+        switch(move){
+            case 1:
+                return (match <= this.atk1.cost);
+            case 2:
+                return (match <= this.atk2.cost);
+            case 3: //potion
+                return(match <= 2);
+            default: //dodge
+                return(match <= 2);
+        }
+    }
+
+    public int move(int move, Entity e){
+        if(!canMove(move))
+            return 0;
+        switch(move){
+            case 1:
+                if(e.dodging){
+                    e.resetDodge();
+                    return 0;
+                }
+                e.takeDamage(this.attack(atk1));
+                return 1;
+            case 2:
+                if(e.dodging){
+                    e.resetDodge();
+                    return 0;
+                }
+                e.takeDamage(this.attack(atk2));
+                return 1;
+            case 3:
+                if(potionCount == 0){
+                    return 0;
+                }
+                this.heal(10);
+                potionCount--;
+                return 1;
+            default:
+                dodge();
+                return 1;
+
+        }
     }
 
     public boolean canEquip(){
@@ -59,6 +107,8 @@ public class Player extends Entity{
     public void equipWeapon(int location){
         if(this.weapon == null){
             this.weapon = equipment.get(location);
+            atk1 = this.weapon.atk1;
+            atk2 = this.weapon.atk2;
             equipment.remove(location);
             return;
             
@@ -66,6 +116,13 @@ public class Player extends Entity{
 
         equipment.add(this.weapon);
         this.weapon = equipment.get(location);
+    }
+
+    public void unequipWeaon(){
+        equipment.add(this.weapon);
+        this.atk1 = null;
+        this.atk2 = null;
+        this.weapon = null;
     }
 
 
