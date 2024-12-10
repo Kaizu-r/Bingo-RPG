507f82a12aa1ebb31c29fa8129283bb4bd0fe940
Kaizu-r <jljoloc@gmail.com>
2024-12-11 02:12:24 +0800

    .class files

--- src/Attack.java
+++ src/Attack.java
@@ -6,7 +6,7 @@ public class Attack{
     public String name;
     public int cost;
 
-    Attack(String name, int damage, int cost){
+    public Attack(String name, int damage, int cost){
         this.name = name;
         this.damage = damage;
         this.cost = cost;
